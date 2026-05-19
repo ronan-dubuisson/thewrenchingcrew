8ff75791a8f9eae48bda6b40d8578b086d2820e5
Ronan Dubuisson <ronan.dubuisson@dubuissonconsulting.be>
2026-05-19 22:59:32 +0200

    initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#11110F] text-[#EFE6D2]">
+      <section className="flex flex-col items-center justify-center h-screen text-center">
+        <h1 className="text-6xl font-bold tracking-wide">About page</h1>
+
+        <p className="mt-6 text-xl text-[#D8C7A6]">Under construction</p>
+
+        <button className="mt-10 px-6 py-3 bg-[#7A4B2B] rounded-md hover:bg-[#9B5C33] transition">
+          Join the Club
+        </button>
+      </section>
+    </main>
+  )
+}
