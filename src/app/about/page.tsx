e3bb5568d6a5c325bb7ce84f64ab6bd362553050
Ronan Dubuisson <ronan.dubuisson@dubuissonconsulting.be>
2026-05-21 23:53:57 +0200

    Feature: Navbar implemenation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
+import { Navbar } from '../../components/navbar/navbar'
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#11110F] text-[#EFE6D2]">
+      <Navbar />
       <section className="flex flex-col items-center justify-center h-screen text-center">
         <h1 className="text-6xl font-bold tracking-wide">About page</h1>
 
